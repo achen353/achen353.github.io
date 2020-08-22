ccfc457bff1d3a0896cce364ae227958b4e7020f
andrewchen018 <andrewchenemail1202@gmail.com>
2020-08-22 23:00:58 +0800

    resume changes

---
+++ precache-manifest.4a89fdceb98d170dd175b2ad36baa9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4370ded88b5a8543511af95fe4d74471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a8597f488f90bcf901",
+    "url": "/static/css/main.65d5438c.chunk.css"
+  },
+  {
+    "revision": "203fe0ee5bc65d871e5d",
+    "url": "/static/js/2.1d434619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d434619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a8597f488f90bcf901",
+    "url": "/static/js/main.c620277b.chunk.js"
+  },
+  {
+    "revision": "2b625de33a9c88376f30",
+    "url": "/static/js/runtime-main.33faa2e7.js"
+  }
+]);
